2e98b2d57de57a9e1d1a842b0cdbe9f6f154de50
0xAckerMan <joelkores1@gmail.com>
2023-05-18 18:51:40 +0300

    Collections: Arrays, Slices, Maps

---
+++ InputOutput/data/collections.go
@@ -0,0 +1,18 @@
+package data
+
+import "fmt"
+
+
+var Countries [10]string
+var Slice []int
+var Codes map[int]string
+
+func init(){
+    Countries[0] = "Kenya"
+    Countries[1] = "Japan"
+    Countries[2] = "USA"
+
+    qty = len(Countries)
+
+    fmt.Println("Countries saved")
+}
