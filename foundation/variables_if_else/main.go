8d54fb760c82f031ed7c93c5c34797933743072a
0xAckerMan <joelkores1@gmail.com>
2023-07-03 23:41:26 +0300

    Main module

---
+++ foundation/variables_if_else/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "variables/go/x00-print_alphabet"
+
+func main (){
+    fmt.Printf("0x00 this is just a test\n")
+    fmt.Println("It works\n\n")
+
+    println(x00printalphabet.IncrementAlpha())
+}
