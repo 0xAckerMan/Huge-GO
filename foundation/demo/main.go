2f9db40340bd634d5dc15412e6bd299be96c5f2f
0xAckerMan <joelkores1@gmail.com>
2023-07-05 09:16:16 +0300

    The main func

--- foundation/demo/main.go
+++ foundation/demo/main.go
@@ -4,19 +4,25 @@ import "fmt"
 
 
 func main(){
-    fmt.Println("Hello from go")
-    fmt.Println(split(6))
+//     fmt.Println("Hello from go")
+//     fmt.Println(split(6))
+    price := 230
+    priceInc(&price)
+    fmt.Println(price)
+// }
+//
+//
+// func split(num int) (int, int) {
+//     half := num/2
+//     squared := num * 2
+//
+//     return half, squared
+
 }
 
 
-func split(num int) (int, int) {
-    half := num/2
-    squared := num * 2
+// func init(){
 
-    return half, squared
-}
-
-
-func init(){
-    fmt.Println("This is just init")
+func priceInc(price *int) {
+    *price ++
 }
