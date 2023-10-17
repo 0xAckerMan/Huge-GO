32c5097291ed52daeeb5caac6997392993fca716
0xAckerMan <joelkores1@gmail.com>
2023-10-17 22:44:04 +0300

    Rectangle Test

---
+++ Go-Almost_a_circle/tests/rectangle_test.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/0xAckerMan/Huge-GO/Go-Almost_a_circle/models"
+)
+
+func TestNewRectangeNoID(t *testing.T){
+    rect := models.NewRectangle(10, 2)
+    if 
+}
