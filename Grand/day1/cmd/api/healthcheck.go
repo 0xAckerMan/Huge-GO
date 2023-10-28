fa06d3e1ff10f263a564ba4782d1da605dc3bc8a
0xAckerMan <joelkores1@gmail.com>
2023-10-28 21:48:52 +0300

    heathchecker

---
+++ Grand/day1/cmd/api/healthcheck.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Application) healthcheck(w http.ResponseWriter, r *http.Request){
+    status := map[string]string{
+        "status":"active",
+        "environment": app.config.env,
+        "version": Version,
+    }
+
+    err := app.writeJSON(w, http.StatusOK, status, nil)
+    if err != nil{
+        http.Error(w, "Internal server error", http.StatusInternalServerError)
+    }
+}
